0ed028c99ecc543b69d6efbd38074415e195f02c
Ryan Murphy <56881027+RyanMurph17@users.noreply.github.com>
2020-02-25 17:22:17 +0000

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+  <template>
+    <div id="app">
+      <ul>
+        <feed v-for="feed in feeds" :feed="feed" :key="feed.title"></feed>
+       </ul>
+    </div>
+  </template>
+
+
+<script>
+import io from 'socket.io-client'
+   import Feed from './components/Feed.vue'
+   export default {
+      components: {
+         Feed
+      },
+      data () {
+         return {
+            feeds: []
+         }
+      },
+      mounted() {
+         this.subscribeToFeed();
+      },
+      methods: {
+         subscribeToFeed() {
+            const socket = io('http://localhost:8081');
+            socket.on('feed', data => {
+               data.feed.entries().forEach(feed => {
+                  this.feeds.push(feed);
+               });
+            });
+         }
+      }
+   }
+</script>
---
+++ src/components/Feed.vue
@@ -0,0 +1,11 @@
+<template>
+   <li>
+      {{feed.title}}
+   </li>
+</template>
+
+<script>
+   export default {
+      props: ["feed"]
+   }
+</script>
